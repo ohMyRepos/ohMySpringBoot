0321c1ccf359fc6c77669e094ab19b029284dcc6
zhanglintc <zhanglintc623@gmail.com>
2024-01-15 22:35:31 +0800

    init

---
+++ src/main/java/co/zhanglintc/ohMySpringBoot/OhMySpringBootApplication.java
@@ -0,0 +1,13 @@
+package co.zhanglintc.ohMySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMySpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhMySpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/zhanglintc/ohMySpringBoot/OhMySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package co.zhanglintc.ohMySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
