7abc263a97aaad98bd3aab0a7513ee9ff89936a4
zhanglintc <zhanglintc623@gmail.com>
2024-04-30 17:24:38 +0800

    use nacos

--- src/main/java/co/zhanglintc/ohMySpringBoot/OhMySpringBootApplication.java
+++ src/main/java/co/zhanglintc/ohMySpringBoot/OhMySpringBootApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 @EnableApolloConfig
+// @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "192.168.33.10:8848"))
+// @NacosPropertySource(dataId = "ohMySpringBoot", autoRefreshed = true)
 public class OhMySpringBootApplication {
 
     public static void main(String[] args) {
